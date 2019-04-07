d44cf7ccb80a320d6a79225bf5ce2bb98e4ccf30
Corbin McKeeth <Corbin@directscale.com>
2019-04-06 19:53:49 -0600

    initialized repo

---
+++ CatalogApp/wwwroot/js/site.js
@@ -0,0 +1,43 @@
+﻿var baseUrl = "localhost:50365";
+
+function displayItems() {
+    var items = getItems();
+
+    for(var item in items)
+    {
+        
+
+    }
+
+
+}
+
+
+
+
+
+
+
+//async functions
+
+function getItems() {
+    var url = baseUrl + "/items";
+    
+
+    return items;
+}
+
+
+function addItem(name, price, desc, image) {
+    var url = baseUrl + "/items";
+
+
+}
+
+
+function removeItem(name) {
+    var url = baseUrl + "/items";
+
+
+
+}
